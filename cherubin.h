abc9c0dbed40b94029dc74c94f271c4b0bc17f26
Julien <pgyygp090@gmail.com>
2024-12-01 15:09:40 +0100

    removed debug cout statements

--- cherubin.h
+++ cherubin.h
@@ -32,23 +32,15 @@ std::string addint(std::string &x, std::string &x2) {
   int cur_val;
   int bf_cnt;
   bool agn;
-  std::cout << "cnt: " << cnt << "\n";
-  std::cout << "cur_str: " << cur_str << "\n";
   if (cur_str[0] != '-') {
     for (int i = cnt; i < sizen; ++i) {
       cur_val = (int(bf_str[i - cnt]) - 48) + (int(cur_str[i]) - 48);
-      std::cout << cur_val << " cur_val\n";
-      std::cout << cur_str << " cur_str \n";
-      std::cout << i << " i\n";
       if (cur_val >= 10) {
         cur_val -= 10;
-        std::cout << cur_val << " cur_val2\n";
         cur_str[i] = char(cur_val + 48);
         bf_cnt = 0;
-        std::cout << "WTF: " << i - bf_cnt << "\n";
         agn = 1;
         while (agn) {
-          std::cout << "ICI\n";
           bf_cnt += 1;
           if (i - bf_cnt > -1) {
             if (int(cur_str[i - bf_cnt]) - 48 > 8) {
@@ -73,3 +65,4 @@ std::string addint(std::string &x, std::string &x2) {
   };
   return cur_str;
 };
+
