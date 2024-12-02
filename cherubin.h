613eee4dcae52acc36f29dd3ea4ec40d21affac9
Julien <pgyygp090@gmail.com>
2024-12-02 16:20:10 +0100

    added addflt function

--- cherubin.h
+++ cherubin.h
@@ -1,7 +1,7 @@
 #include <iostream>
 
 //@L Addition
-//@L2 Int
+//@L2 Integers
 
 //@T addint
 //@U std::string addint(std::string &x, std::string &x2)
@@ -72,8 +72,126 @@ std::string addint(std::string &x, std::string &x2) {
   return cur_str;
 };
 
+//@L2 Floating points
+
+//@T addflt
+//@U std::string addflt(std::string &x, std::string &x2)
+//@X
+//@D Returns the addition of two floating points as a std string. Accepts integers, but if you plan to add integers, <code>addint</code> is preferable to use.
+//@A 
+//@A
+//@E
+//@X
+
+std::string addflt(std::string &x, std::string &x2) {
+  std::string cur_str;
+  std::string bf_str;
+  const unsigned bf_cn = x.length();
+  const unsigned bf_cn2 = x2.length();
+  unsigned int cnt = 0;
+  bool agn = 1;
+  bool is_dec1 = 0;
+  bool is_dec2 = 0;
+  int i;
+  unsigned int cur_delta = 0;
+  while (agn) {
+    cnt += 1;
+    if (cnt == bf_cn) {
+      agn = 0;
+    } else if (x[cnt] == '.') {
+      is_dec1 = 1;
+      agn = 0;
+    };
+  };
+  const unsigned int n = cnt;
+  agn = 1;
+  cnt = 0;
+  while (agn) {
+    cnt += 1;
+    if (cnt == bf_cn2) {
+      agn = 0;
+    } else if (x2[cnt] == '.') {
+      is_dec2 = 1;
+      agn = 0;
+    };
+  };
+  const unsigned int n2 = cnt;
+  unsigned int sizen;
+  if (n2 > n) {
+    cur_str = x2;
+    bf_str = x;
+    cnt = n2 - n;
+    cur_delta = (bf_cn - n) - (bf_cn2 - n2);
+    if (cur_delta > 0) {
+      sizen = bf_cn2 + cur_delta;
+      if (!is_dec2) {
+        cur_delta -= 1;
+        cur_str += ".";
+      };
+      for (i = 0; i < cur_delta; ++i) {
+        cur_str += "0";
+      };
+    };
+  } else {
+    cur_str = x;
+    bf_str = x2;
+    cnt = n - n2; 
+    cur_delta = (bf_cn2 - n2) - (bf_cn - n);
+    if (cur_delta > 0) {
+      sizen = bf_cn + cur_delta;
+      if (!is_dec1) {
+        cur_delta -= 1;
+        cur_str += ".";
+      };
+      for (i = 0; i < cur_delta; ++i) {
+        cur_str += "0";
+      };
+    };
+  };
+  int cur_val;
+  int bf_cnt;
+  if (bf_str[0] == '-') {
+    return "";
+  };
+  if (cur_str[0] != '-') {
+    for (i = cnt; i < sizen; ++i) {
+      if (cur_str[i] != '.') {
+        cur_val = (int(bf_str[i - cnt]) - 48) + (int(cur_str[i]) - 48);
+        if (cur_val > 9) {
+          cur_val -= 10;
+          cur_str[i] = char(cur_val + 48);
+          bf_cnt = 0;
+          agn = 1;
+          while (agn) {
+            bf_cnt += 1;
+            if (i - bf_cnt > -1) {
+              if (int(cur_str[i - bf_cnt]) - 48 > 8) {
+                cur_str[i - bf_cnt] = '0';
+              } else {
+                cur_str[i - bf_cnt] = char(int(cur_str[i - bf_cnt]) + 1);
+                agn = 0;
+              };
+            } else {
+              cur_str = "1" + cur_str;
+              bf_str = "1" + bf_str;
+              i += 1;
+              sizen += 1;
+              agn = 0;
+            };
+          };
+        } else {
+          cur_str[i] = char(int(cur_val) + 48);
+        };
+      };
+    };
+  } else {
+    return "";
+  };
+  return cur_str;
+};
+
 //@L Substraction
-//@L2 Int
+//@L2 Integers
 
 //@T subsint
 //@U std::string subsint(std::string &x, std::string &x2)
