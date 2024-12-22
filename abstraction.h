4e53d92d39b670e08dc1a57130455b83477eb437
Julien <pgyygp090@gmail.com>
2024-12-22 14:37:11 +0100

    added mult/divideabstraction functions

--- abstraction.h
+++ abstraction.h
@@ -5,7 +5,9 @@ std::string addabstraction(std::string x, std::string x2) {
   bool grt;
   const unsigned int n = x.length();
   const unsigned int n2 = x2.length();
-  if (x[0] == '-' & x2[0] == '-') {
+  if (x == x2) {
+    return "0";
+  } else if (x[0] == '-' & x2[0] == '-') {
     rtn_str = "-";
     x = x.substr(1, n);
     x2 = x2.substr(1, n2);
@@ -36,7 +38,7 @@ std::string addabstraction(std::string x, std::string x2) {
   };
 };
 
-std::string chexp2(std::string &x, unsigned int nb_polynomial = 20) {
+std::string chexp2(std::string &x, unsigned int nb_polynomial = 21) {
   std::string rtn_str = "1";
   std::string addr = "1";
   std::string ref_add = "1";
@@ -46,11 +48,57 @@ std::string chexp2(std::string &x, unsigned int nb_polynomial = 20) {
   for (unsigned int i = 1; i < nb_polynomial; ++i) {
     numerator = powerint3(x, addr);
     denumerator = factorial(addr);
-    std::cout << "numerator: " << numerator << "\n";
-    std::cout << "denumerator: " << denumerator << "\n";
     rslt = divide2(numerator, denumerator, 10);
     rtn_str = addflt(rtn_str, rslt);
     addr = addint(addr, ref_add);
   };
   return rtn_str;
 };
+
+
+std::string multabstraction(std::string x, std::string x2) {
+  std::string rtn_str;
+  const unsigned int n = x.length();
+  const unsigned int n2 = x2.length();
+  if (x[0] == '-' & x2[0] == '-') {
+    x = x.substr(1, n);
+    x2 = x2.substr(1, n2);
+    return multflt(x, x2);
+  } else if (x[0] == '-') {
+    rtn_str = "-";
+    x = x.substr(1, n);
+    rtn_str += multflt(x, x2);
+    return rtn_str;
+  } else if (x2[0] == '-') {
+    rtn_str = "-";
+    x2 = x2.substr(1, n2);
+    rtn_str += multflt(x, x2);
+    return rtn_str;
+  } else {
+    return multflt(x, x2);
+  };
+};
+
+std::string divideabstraction(std::string x, std::string x2) {
+  std::string rtn_str;
+  const unsigned int n = x.length();
+  const unsigned int n2 = x2.length();
+  if (x[0] == '-' & x2[0] == '-') {
+    x = x.substr(1, n);
+    x2 = x2.substr(1, n2);
+    return divide2(x, x2);
+  } else if (x[0] == '-') {
+    rtn_str = "-";
+    x = x.substr(1, n);
+    rtn_str += divide2(x, x2);
+    return rtn_str;
+  } else if (x2[0] == '-') {
+    rtn_str = "-";
+    x2 = x2.substr(1, n2);
+    rtn_str += divide2(x, x2);
+    return rtn_str;
+  } else {
+    return divide2(x, x2);
+  };
+};
+
