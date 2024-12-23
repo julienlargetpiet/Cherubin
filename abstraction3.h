c6d971ce93e5b26267224c92fc5e29230d5bb171
Julien <pgyygp090@gmail.com>
2024-12-23 18:56:18 +0100

    added inverse function

---
+++ abstraction3.h
@@ -0,0 +1,18 @@
+#include "abstraction2.h"
+
+//@T inverse
+//@U std::string inverse(std::string &a, std::string &b, std::string (*f)(std::string&, std::string&))
+//@X
+//@D Returns the inverse of the result of a function passed as a parameter.
+//@A f : is the function passed as a parameter
+//@A a : is the first number, as a std string, of the function passed as a parameter
+//@A b : is the second number, as a std string, of the function passed as a parameter
+//@X
+//@E
+//@X
+
+std::string inverse(std::string &a, std::string &b, std::string (*f)(std::string&, std::string&)) {
+  std::string x = "1";
+  std::string rtn_str = f(a, b);
+  return divide2(x, rtn_str);
+};
