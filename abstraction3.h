2bbf3623dd15845e86664fc7af7650fe19dc053f
Julien <pgyygp090@gmail.com>
2024-12-23 20:26:35 +0100

    added powerflt function

--- abstraction3.h
+++ abstraction3.h
@@ -16,3 +16,22 @@ std::string inverse(std::string &a, std::string &b, std::string (*f)(std::string
   std::string rtn_str = f(a, b);
   return divide2(x, rtn_str);
 };
+
+
+//@T powerflt
+//@U std::string powerflt(std::string &x, std::string &x2, unsigned int nb_polynomial = 20)
+//@X
+//@D Returns the result of a int or float (as std string) to the power of int or float (as std string) 
+//@A x : is the number (int or float as std string) that will be elevated to an exponent
+//@A x2 : is the exponent (int or float as std string)
+//@A nb_polynomial : is the length of the polynomial used for the taylor serie of exp(x) that allow the function to work.
+//@E
+//@X
+
+std::string powerflt(std::string &x, std::string &x2, unsigned int nb_polynomial = 20) {
+  std::string rtn_str = chlognatural1(x);
+  rtn_str = multflt2(rtn_str, x2);
+  return chexp2(rtn_str, nb_polynomial);
+};
+
+
