2dcd27c28f19f5ed5544dcb7802c45128fb37eed
Julien <pgyygp090@gmail.com>
2024-12-23 14:57:45 +0100

    fixed error in chlognatural1 function

--- abstraction.h
+++ abstraction.h
@@ -1,4 +1,5 @@
 #include "cherubin.h"
+#include <math.h>
 
 std::string addabstraction(std::string x, std::string x2) {
   std::string rtn_str;
@@ -37,24 +38,6 @@ std::string addabstraction(std::string x, std::string x2) {
     return addflt(x, x2);
   };
 };
-
-std::string chexp2(std::string &x, unsigned int nb_polynomial = 21) {
-  std::string rtn_str = "1";
-  std::string addr = "1";
-  std::string ref_add = "1";
-  std::string rslt;
-  std::string numerator;
-  std::string denumerator;
-  for (unsigned int i = 1; i < nb_polynomial; ++i) {
-    numerator = powerint3(x, addr);
-    denumerator = factorial(addr);
-    rslt = divide2(numerator, denumerator, 10);
-    rtn_str = addflt(rtn_str, rslt);
-    addr = addint(addr, ref_add);
-  };
-  return rtn_str;
-};
-
 
 std::string multabstraction(std::string x, std::string x2) {
   std::string rtn_str;
@@ -100,5 +83,45 @@ std::string divideabstraction(std::string x, std::string x2) {
   } else {
     return divide2(x, x2);
   };
+};
+
+
+std::string chexp2(std::string &x, unsigned int nb_polynomial = 21) {
+  std::string rtn_str = "1";
+  std::string addr = "1";
+  std::string ref_add = "1";
+  std::string rslt;
+  std::string numerator;
+  std::string denumerator;
+  for (unsigned int i = 1; i < nb_polynomial; ++i) {
+    numerator = powerint3(x, addr);
+    denumerator = factorial(addr);
+    rslt = divide2(numerator, denumerator, 10);
+    rtn_str = addflt(rtn_str, rslt);
+    addr = addint(addr, ref_add);
+  };
+  return rtn_str;
+};
+
+std::string chlognatural1(std::string &x) {
+  std::string base_natural = "2.718281";
+  std::string cbase_natural = base_natural;
+  std::string lst_str;
+  std::string intr_str;
+  std::string rslt;
+  std::string base_add = "0";
+  std::string addr = "1";
+  double cur_val;
+  while (is_greaterflt(x, base_natural)) {
+    lst_str = base_natural;
+    base_natural = multflt2(base_natural, cbase_natural);
+    base_add = addint(base_add, addr);
+  };
+  rslt = divide2(x, lst_str);
+  cur_val = chstod(rslt);
+  cur_val = log(cur_val);
+  intr_str = std::to_string(cur_val);
+  base_add = addflt(base_add, intr_str);
+  return base_add;
 };
 
