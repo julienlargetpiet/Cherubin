bdfcf187269bbb7b03ad180acdbdf6c3741ed324
Julien <pgyygp090@gmail.com>
2024-12-25 17:41:49 +0100

    added subsflt function

--- cherubindq.h
+++ cherubindq.h
@@ -331,6 +331,135 @@ std::deque<char> subsint(std::deque<char> &x, std::deque<char> &x2) {
   return cur_dq;
 };
 
+std::deque<char> subsflt(std::deque<char> &x, std::deque<char> &x2) {
+  std::deque<char> cur_dq = x;
+  std::deque<char> bf_dq = x2;
+  const unsigned bf_cn = x.size();
+  const unsigned bf_cn2 = x2.size();
+  unsigned int cnt = 0;
+  bool agn = 1;
+  bool is_dec1 = 0;
+  bool is_dec2 = 0;
+  int i;
+  int cur_delta = 0;
+  while (agn) {
+    cnt += 1;
+    if (cnt == bf_cn) {
+      agn = 0;
+    } else if (x[cnt] == '.') {
+      is_dec1 = 1;
+      agn = 0;
+    };
+  };
+  const unsigned int n = cnt;
+  agn = 1;
+  cnt = 0;
+  while (agn) {
+    cnt += 1;
+    if (cnt == bf_cn2) {
+      agn = 0;
+    } else if (x2[cnt] == '.') {
+      is_dec2 = 1;
+      agn = 0;
+    };
+  };
+  const unsigned int n2 = cnt;
+  unsigned int sizen;
+  if (n2 > n) {
+    cur_dq = x2;
+    bf_dq = x;
+    cnt = n2 - n;
+    cur_delta = (bf_cn - n) - (bf_cn2 - n2);
+    sizen = bf_cn2;
+    if (cur_delta > 0) {
+      sizen += cur_delta;
+      if (!is_dec2) {
+        cur_delta -= 1;
+        cur_dq.push_back('.');
+      };
+      for (i = 0; i < cur_delta; ++i) {
+        cur_dq.push_back('0');
+      };
+    } else if (cur_delta < 0) {
+      cur_delta *= -1;
+      if (!is_dec1) {
+        cur_delta -= 1;
+        bf_dq.push_back('.');
+      };
+      for (i = 0; i < cur_delta; ++i) {
+        bf_dq.push_back('0');
+      };
+    };
+  } else {
+    cur_dq = x;
+    bf_dq = x2;
+    cnt = n - n2; 
+    cur_delta = (bf_cn2 - n2) - (bf_cn - n);
+    sizen = bf_cn;
+    if (cur_delta > 0) {
+      sizen += cur_delta;
+      if (!is_dec1) {
+        cur_delta -= 1;
+        cur_dq.push_back('.');
+      };
+      for (i = 0; i < cur_delta; ++i) {
+        cur_dq.push_back('0');
+      };
+    } else if (cur_delta < 0) {
+      cur_delta *= -1;
+      if (!is_dec2) {
+        cur_delta -= 1;
+        bf_dq.push_back('.');
+      };
+      for (i = 0; i < cur_delta; ++i) {
+        bf_dq.push_back('0');
+      };
+    };
+  };
+  int cur_val;
+  int bf_cnt;
+  if (bf_dq[0] == '-' || cur_dq[0] == '-') {
+    return cur_dq;
+  };
+  for (i = cnt; i < sizen; ++i) {
+    if (cur_dq[i] != '.') {
+      cur_val = (int(cur_dq[i]) - 48) - (int(bf_dq[i - cnt]) - 48);
+      if (cur_val < 0) {
+        cur_val = 10 + cur_val;
+        cur_dq[i] = char(int(cur_val) + 48);
+        if (cur_dq[i - 1] != '.') {
+          bf_cnt = 0;
+        } else {
+          bf_cnt = 1;
+        };
+        agn = 1;
+        while (agn) {
+          bf_cnt += 1;
+          if (cur_dq[i - bf_cnt] != '.') {
+            if (cur_dq[i - bf_cnt] == '0') {
+              cur_dq[i - bf_cnt] = '9';
+            } else {
+              cur_dq[i - bf_cnt] = char(int(cur_dq[i - bf_cnt]) - 1);
+              agn = 0;
+            };
+          };
+        };
+      } else {
+        cur_dq[i] = char(cur_val + 48);
+      };
+      if (cur_dq[0] == '0' & sizen > 1) {
+        if (cur_dq[1] != '.') {
+          cur_dq.pop_front();
+          bf_dq.pop_front();
+          i -= 1;
+          sizen -= 1;
+        };
+      };
+    };
+  };
+  return cur_dq;
+};
+
 std::string dqtostr(std::deque<char> &x) {
   std::string cur_str = "";
   for (char i : x) {
