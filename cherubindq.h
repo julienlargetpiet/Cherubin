8ceae151834e6b53e2eb62251a71b731e637eac4
Julien <pgyygp090@gmail.com>
2024-12-25 21:32:16 +0100

    added multflt2 function

--- cherubindq.h
+++ cherubindq.h
@@ -507,6 +507,228 @@ std::deque<char> multint(std::deque<char> &x, unsigned int &x2) {
   return cur_dq;
 };
 
+std::deque<char> multint2(std::deque<char> &x, std::deque<char> &x2) {
+  std::deque<char> cur_dq = x;
+  std::deque<char> cur_dq2 = x;
+  std::deque<char> bf_dq = x2;
+  int cnt = 0;
+  std::deque<char> eval_dq = {'1'};
+  unsigned int n = x.size();
+  unsigned int n2 = x2.size();
+  int cur_val;
+  unsigned int delta = 0;
+  int cur_eval;
+  int bf_cnt;
+  bool agn;
+  int i;
+  if (bf_dq[0] == '-' || cur_dq[0] == '-') {
+    return cur_dq;
+  };
+  while (eval_dq != bf_dq) {
+    for (i = delta; i < n; ++i) {
+      cur_val = (int(cur_dq[i]) - 48) + (int(cur_dq2[i - delta]) - 48);
+      if (cur_val > 9) {
+        cur_val -= 10;
+        cur_dq[i] = char(cur_val + 48);
+        bf_cnt = 0;
+        agn = 1;
+        while (agn) {
+          bf_cnt += 1;
+          if (i - bf_cnt > -1) {
+            if (cur_dq[i - bf_cnt] == '9') {
+              cur_dq[i - bf_cnt] = '0';
+            } else {
+              cur_dq[i - bf_cnt] = char(int(cur_dq[i - bf_cnt]) + 1);
+              agn = 0;
+            };
+          } else {
+            cur_dq.push_front('1');
+            i += 1;
+            delta += 1;
+            n += 1;
+            agn = 0;
+          };
+        };
+      } else {
+        cur_dq[i] = char(cur_val + 48);
+      };
+    };
+    cur_eval = int(eval_dq[cnt]) - 48;
+    if (cur_eval > 8) {
+      eval_dq[cnt] = '0';
+      agn = 1;
+      bf_cnt = 0;
+      while (agn) {
+        bf_cnt += 1;
+        if (cnt - bf_cnt > -1) {
+          if (eval_dq[cnt - bf_cnt] == '9') {
+            eval_dq[cnt - bf_cnt] = '0';
+          } else {
+            eval_dq[cnt - bf_cnt] = char(int(eval_dq[cnt - bf_cnt]) + 1);
+            agn = 0;
+          };
+        } else {
+          eval_dq.push_front('1');
+          cnt += 1;
+          agn = 0;
+        };
+      };
+    }else {
+      eval_dq[cnt] = char(cur_eval + 49);
+    };
+  };
+  return cur_dq;
+};
+
+std::deque<char> multflt2(std::deque<char> &x, std::deque<char> &x2) {
+  int i;
+  int i2;
+  unsigned int i3;
+  int n = x.size();
+  int n2 = x2.size();
+  bool agn;
+  int add_zero = 0;
+  int cur_val;
+  int intr_val;
+  int bf_cnt;
+  int delta = 0;
+  unsigned int delta2 = 0;
+  std::deque<char> divider;
+  std::deque<char> rtn_dq;
+  std::deque<char> rtn_dq2;
+  std::deque<char> rtn_dq3;
+  if (n > 2) {
+    agn = 1;
+    i = 0;
+    while (agn) {
+      rtn_dq.push_back(x[i]);
+      i += 1;
+      if (i + 1 == n) {
+        agn = 0;
+      } else if (x[i] == '.'){
+        agn = 0;
+      };
+    };
+    if (i + 1 < n) {
+      add_zero = n - 1 - i;
+      i += 1;
+      while (i < n) {
+        rtn_dq.push_back(x[i]);
+        i += 1;
+      };
+      if (x[0] != '0') {
+        n -= 1;
+      } else {
+        n -= 2;
+        rtn_dq.pop_front();
+      };
+    } else {
+      rtn_dq = x;
+    };
+  } else {
+    rtn_dq = x;
+  };
+  if (n2 > 2) {
+    agn = 1;
+    i = 0;
+    while (agn) {
+      rtn_dq3.push_back(x2[i]);
+      i += 1;
+      if (i + 1 == n2) {
+        agn = 0;
+      } else if (x2[i] == '.'){
+        agn = 0;
+      };
+    };
+    if (i + 1 < n2) {
+      add_zero += n2 - 1 - i;
+      i += 1;
+      while (i < n2) {
+        rtn_dq3.push_back(x2[i]);
+        i += 1;
+      };
+      if (x2[0] != '0') {
+        n2 -= 1;
+      } else {
+        n2 -= 2;
+        rtn_dq3.pop_front();
+      };
+    } else {
+      rtn_dq3 = x2;
+    };
+  } else {
+    rtn_dq3 = x2; 
+  };
+  for (i = 1; i < n + n2; ++i) {
+    rtn_dq2.push_back('0');
+  };
+  cur_val = int(rtn_dq3[0] - 48) * int(rtn_dq[0] - 48);
+  if (cur_val > 9) {
+    rtn_dq2.push_back('0');
+    n2 += 1;
+    delta2 = 1;
+  };
+  for (i3 = 1; i3 < add_zero; ++i3) {
+    rtn_dq2.push_back('0');
+    n2 += 1;
+    delta += 1;
+  };
+  for (i2 = delta + delta2; i2 < n2; ++i2) {
+    for (i = 0; i < n; ++i) {
+      cur_val = int(rtn_dq2[i2 + i - delta] - 48) + int(rtn_dq3[i2 - delta - delta2] - 48) * int(rtn_dq[i] - 48);
+      if (cur_val < 10) {
+        rtn_dq2[i2 + i - delta] = char(cur_val + 48);
+      } else {
+        intr_val = 1;
+        while (cur_val - (intr_val + 1) * 10 > -1) {
+          intr_val += 1;
+        };
+        cur_val -= intr_val * 10;
+        rtn_dq2[i2 + i - delta] = char(cur_val + 48);
+        cur_val = int(rtn_dq2[i2 + i - delta - 1] - 48) + intr_val;
+        if (cur_val < 10) {
+          rtn_dq2[i2 + i - delta - 1] = char(cur_val + 48);   
+        } else {
+          cur_val -= 10;
+          rtn_dq2[i2 + i - delta - 1] = char(cur_val + 48);
+          agn = 1;
+          bf_cnt = 0;
+          while (agn) { 
+            bf_cnt += 1;
+            if (i2 + i - delta - bf_cnt - 1 > - 1) {
+              if (rtn_dq2[i2 + i - delta - bf_cnt - 1] == '9') {
+                rtn_dq2[i2 + i - delta - bf_cnt - 1] = '0'; 
+              } else {
+                rtn_dq2[i2 + i - delta - bf_cnt - 1] = char(int(rtn_dq2[i2 + i - delta - bf_cnt - 1]) + 1); 
+                agn = 0;
+              };
+            } else {
+              delta2 += 1;
+              i2 += 1;
+              n2 += 1;
+              rtn_dq2.push_front('1');
+              agn = 0;
+            };
+          };
+        };
+      };
+    };
+  };
+  if (add_zero != 0) {
+    delta = n2 + n - add_zero * 2;
+    if (delta > 0){
+      rtn_dq2.insert(rtn_dq2.begin() + delta, '.');
+    } else {
+      rtn_dq2.push_front('.');
+      rtn_dq2.push_front('0');
+      for (i = delta; i < 0; ++i) {
+        rtn_dq2.insert(rtn_dq2.begin() + 2, '0');
+      };
+    };
+  };
+  return rtn_dq2;
+};
+
 std::string dqtostr(std::deque<char> &x) {
   std::string cur_str = "";
   for (char i : x) {
