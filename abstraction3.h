0c3040d51ea9b8a65ef753c60172759584394a34
Julien <pgyygp090@gmail.com>
2025-01-05 13:39:24 +0100

    added chcos function

--- abstraction3.h
+++ abstraction3.h
@@ -44,4 +44,42 @@ std::string powerflt(std::string &x, std::string &x2) {
   return chexp3(rtn_str);
 };
 
+//@T chcos
+//@U std::string chcos(std::string &x, std::string base = "3.141593", unsigned int nb_polynom = 10)
+//@X
+//@D Returns the result of cosinus(x) with the help of taylor serie
+//@A x : is the input number, int or float as std string
+//@A base : is the closest value of pi
+//@A nb_polynom : is the ssize of the polynom used for the approximation
+//@X
+//@E std::string x = "-125.5";
+//@E std::string final_rslt = chcos(x);
+//@E -0.9866278020907 
+//@X
+
+std::string chcos(std::string &x, std::string base = "3.141593", unsigned int nb_polynom = 10) {
+  std::string new_x = x;
+  if (new_x[0] == '-') {
+    new_x = new_x.substr(1, x.length());
+  };
+  new_x = remainderflt(new_x, base);
+  std::string rslt = "1";
+  std::string cur_divided;
+  std::string cur_divider;
+  std::string addr = "2";
+  std::string cur_add = "2";
+  std::string cur_rslt;
+  for (unsigned int i = 1; i < nb_polynom; ++i) {
+    cur_divided = powerint3(new_x, cur_add);
+    cur_divider = factorial(cur_add);
+    cur_rslt = divide2(cur_divided, cur_divider);
+    if (i % 2 == 0) {
+      rslt = addabstraction(rslt, cur_rslt);
+    } else {
+      rslt = subsabstraction(rslt, cur_rslt);
+    };
+    cur_add = addint(cur_add, addr);
+  };
+  return rslt;
+};
 
