7807d76588f8ccf9e33eced1850f7c72c305eaf1
Julien <pgyygp090@gmail.com>
2025-01-08 20:12:40 +0100

    updated doc

--- cherubin.h
+++ cherubin.h
@@ -2,7 +2,6 @@
 #include <vector>
 
 //@D This library if for very large and precise number (integers or floating points) manipulation those who can not be stored in an other datatype than <code>std::string</code> or <code>std::deque</code>. The only limit is the system memory.
-//@D This branch is dedicated to <code>std::string</code> datatype for storing and manipulating numbers, contrary on the other branch (which is in development) that uses <code>std::deque<char></code> for the same purposes. This branch is more appropriate for medium sized number manipulation and the need for a lot of number declaration since a number declaration in the other branch will copy the elements of the declared string (also starting with string for convenience) to a deque at a cost of little computation each time. However, the other branch functions (in development) will be significantly faster especially for exponential functions.
 
 //@M docimage.jpg
 
